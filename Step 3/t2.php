5c6686f23f1cd7c71c6ee2880f69fba858ac7e06
peterkarney1 <peter@karneyonline.com>
2014-06-29 21:56:00 +0100

    Completed the lesson 3

--- Step 3/t2.php
+++ Step 3/t2.php
@@ -1,15 +1,36 @@
 ﻿<?php
+
+# A welcome greeting
 $greeting="Welcome to devonshire house app lab";
+
+# A bit of text
 $message="The time now is";
+
+# Get the time
 $time = time();
+# Convert the time into something understandable
 $formatted_time = gmdate("H:i:s",$time);
+
+#Goodby message
+$goodbye="This app was build by Adrian";
+#Lets add our names
+
 header('Content-type: text/xml');
 echo "<?xml version='1.0' encoding='UTF-8'?>";
 	echo "<Response>";
- 		echo "<Say>Welcome to clean call debug</Say>";
  		echo "<Say>$greeting</Say>";
  		echo "<Say>$message</Say>";
- 		# echo "<Say>The time now is</Say>";
  		echo "<Say>$formatted_time</Say>";
+ 		echo "<Say>$goodbye</Say>";
 	echo "</Response>";
+
+#lets try making some changes
+# try echo "<Say voice='alice'>This is Alices voice</Say>";
+# try echo "<Say language='es-ES'>This is someone with a spanish accent</Say>";
+# try echo "<Say voice='alice' language='en-GB'>This is a posh english accent</Say>";
+# try echo "<Play loop='1'>https://api.twilio.com/cowbell.mp3</Play>";
+# try echo "<Play loop='1'>http://www.dhsapplab.co/song.mp3</Play>";
 ?>
+
+
+
